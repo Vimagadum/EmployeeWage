afbe1549c3794280054e659db2243fafa6a45df2
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-01-15 20:17:27 +0530

    [Vinayak] adding while loop program

---
+++ EmpWhile.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class EmpWhile
+    {
+        public void Employeeee()
+        {
+            const int Full_Time = 1;
+            const int Part_Time = 2;
+            int empHrs = 0;
+            int totalHrs = 0;
+            int empWagePrHr = 20;
+            int EmpWage = 0;
+            int total_Wage = 0;
+            int WorkingHrs = 0;
+            int Max_Working_Days = 20;
+            int Max_Working_Hrs = 100;
+            int days = 1;
+            Random random = new Random();
+            days = 1;
+            while(days <= Max_Working_Days && WorkingHrs <= Max_Working_Hrs)
+            {
+                int RandomNumber = random.Next(0, 3);
+                if (RandomNumber == Full_Time)
+                {
+                    Console.WriteLine("Emplyee worked for full time");
+                    empHrs = 8;
+                }
+                else if (RandomNumber == Part_Time)
+                {
+                    Console.WriteLine("Emplyee worked for part time");
+                    empHrs = 4;
+                }
+                else
+                {
+                    Console.WriteLine("Emplyee is absent");
+                }
+                
+                EmpWage = empHrs * empWagePrHr;
+                Console.WriteLine("EMployee wage for day {0} is {1}", days, EmpWage);
+                total_Wage += EmpWage;
+                totalHrs += empHrs;
+                days++;
+
+            }
+            Console.WriteLine("totalHrs wage of employee for {0} days is {1} hours is {2}", Max_Working_Days, totalHrs, total_Wage);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EmployeeWage;
+
+
+Console.WriteLine("Hello, World!");
+
+EmpPresentOrNot emp = new EmpPresentOrNot();
+emp.IffElse();
+
+FullTime flt = new FullTime();
+flt.EmployeFullTime();
+
+PartTime prt = new PartTime();
+prt.EmployePartTime();
+
+EmpSwitch empswtch = new EmpSwitch();
+empswtch.Employe();
+
+EmpForLoop emploop = new EmpForLoop();
+emploop.Employeee();
+
+
+EmpWhile empwhile = new EmpWhile();
+empwhile.Employeeee();
