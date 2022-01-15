6e47c14a45103e6cb03828abfe451a6602a3c569
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-01-15 20:14:34 +0530

    [Vinayak] adding switch case program

---
+++ EmpSwitch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class EmpSwitch
+    {
+        public void Employe()
+        {
+            const int Full_Time = 1;
+            const int Part_Time = 2;
+            int empHrs = 0;
+            int empWagePrHr = 20;
+            int total_Wage = 0;
+            Random random = new Random();
+            int RandomNumber = random.Next(0, 3);
+            switch(RandomNumber)
+            {
+                case Full_Time:
+                    empHrs = 8;
+                    Console.WriteLine(" Employee is worked for full time");
+                    break;
+
+                    case Part_Time:
+                    empHrs = 4;
+                    Console.WriteLine("Employee is worked for part time");
+                    break;
+
+                    default:
+                    Console.WriteLine("Employee is absent");
+                    break;
+            }
+
+            total_Wage = empHrs * empWagePrHr;
+            Console.WriteLine("Employee wage is" + total_Wage);
+        }
+    }
+}
