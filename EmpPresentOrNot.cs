deb0217a0e9c88bcb16963e1df40b8cd62bafff1
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-01-15 20:06:02 +0530

    [Vinayak] Employee present oro not program

---
+++ EmpPresentOrNot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class EmpPresentOrNot
+    {
+        
+        public void IffElse()
+        {
+            Random random = new Random();
+            
+            int RandomNumber = random.Next(0, 2);
+
+
+            if (RandomNumber == 0)
+            {
+                Console.WriteLine("Emplyee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Emplyee is absent");
+            }
+
+        }
+        
+
+    }
+}
